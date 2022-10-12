65f6e1f9bb6ebfa183dd92b22404e324b45d97f8
chithian <chithiansamony@gmail.com>
2022-10-12 15:08:06 +0700

    implement Email Validator

--- Package.swift
+++ Package.swift
@@ -10,6 +10,9 @@ let package = Package(
         .library(
             name: "Greeting",
             targets: ["Greeting"]),
+        .library(
+            name: "EmailValidator",
+            targets: ["EmailValidator"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -20,6 +23,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Greeting",
+            dependencies: []),
+        .target(
+            name: "EmailValidator",
             dependencies: []),
      
     ]
