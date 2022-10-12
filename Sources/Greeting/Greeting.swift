0f540eb67ace6f26a1c1680a11c9498bc4de09df
chithian <chithiansamony@gmail.com>
2022-10-12 11:00:03 +0700

    Fix Greeting module

---
+++ Sources/Greeting/Greeting.swift
@@ -0,0 +1,6 @@
+public struct IGLibrary {
+    public private(set) var text = "Hello, World Swift Module!"
+
+    public init() {
+    }
+}
--- Sources/IGLibrary/IGLibrary.swift
+++
@@ -1,6 +0,0 @@
-public struct IGLibrary {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
